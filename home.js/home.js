40ee2ccddfea2a0f0970466c9fb0ec34ce1cc0bf
flowberrys <156359194+flowberrys@users.noreply.github.com>
2025-11-20 14:27:08 -0500

    added button

---
+++ home.js/home.js
@@ -0,0 +1,11 @@
+function btn_over(which) {
+    document.getElementById(which).style.transform="scale(1.2) roatate(2deg)";
+}
+
+function btn_out(which) {
+    document.getElementById(which).style.transform="scale(1.0)";
+}
+
+function btn_down(which) {
+    document.getElementById(which).style.transform="scale(.9)";
+}
